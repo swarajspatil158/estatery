927ef152f4309a0e2bfc29ec43a4264d08b95571
Swaraj Patil <swarajspatil158@gmail.com>
2022-07-07 13:51:35 +0530

    banner header done

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
-
+import React from "react";
+import CountryDropdown from "./CountryDropdown";
+import PropertyDropdown from "./PropertyDropdown";
+import PriceRangeDropdown from "./PriceRangeDropdown";
+import { RiSearch2Line } from "react-icons/ri";
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="px-[30px] py-6 max-w-[1170px] mx-auto flex flex-col lg:flex-row justify-between gap-4 lg:gap-x-3 relative lg:-top-4 lg:shadow-1 bg-white lg:bg-transparent lg:backdrop-blur rounded-lg">
+      <CountryDropdown />
+      <PropertyDropdown />
+      <PriceRangeDropdown />
+      <button>
+        <RiSearch2Line />
+      </button>
+    </div>
+  );
 };
 
 export default Search;
